5eda7e71d87e7b781ba435af837662d3c264c914
prajwol <prajwolghimire19@gmail.com>
2023-12-20 15:53:09 +0545

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/index.js';
 import HeroSection from './components/HeroSection/index.js';
 import Skills from './components/Skills/index.js';
 import Education from './components/Education/index.js';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -22,14 +23,16 @@ const Wrapper = styled.div`
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-        <Navbar />
-        <Body>
-          <HeroSection /> 
-          <Wrapper>
-            <Skills />
-            <Education />
-          </Wrapper>
-        </Body>
+        <Router>
+          <Navbar />
+          <Body>
+            <HeroSection /> 
+            <Wrapper>
+              <Skills />
+              <Education />
+            </Wrapper>
+          </Body>
+        </Router>  
     </ThemeProvider>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,5 @@
 import { GitHub } from '@mui/icons-material';
 import React from 'react'
-import { NavLink } from 'react-router-dom';
 import { Link as LinkR } from 'react-router-dom';
 import styled from 'styled-components'
 
@@ -68,6 +67,57 @@ const NavItems = styled.ul`
 
     @media screen and (max-width: 768px) {
       display: none;
+    }
+`;
+
+const NavLink = styled.a`
+    color: ${({ theme }) => theme.text_primary};
+    font-weight: 500;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+    :hover {
+      color: ${({ theme }) => theme.primary};
+    }
+
+    &.active {
+      border-bottom: 2px solid ${({ theme }) => theme.primary};
+    }
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;  
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+
+const GitHubButton = styled.a`
+  border: 1.8px solid ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 70%;
+  border-radius: 20px;
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  padding: 0 20px;
+  font-weight: 500;
+  text-decoration: none;
+  font-size: 16px;
+  transition: all 0.6s ease-in-out;
+    :hover {
+      background: ${({ theme }) => theme.primary};
+      color: ${({ theme }) => theme.white};     
+    }
+    @media screen and (max-width: 768px) { 
+    font-size: 14px;
     }
 `;
 
