12033a029138b9c4ece31dd6e47263c943c9b187
prajwol-ghimire <prajwolghimire19@gmail.com>
2023-12-21 12:59:08 -0500

    Constants changes

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react'
+import { Container, Wrapper, Title, Desc, SkillsContainer, Skill, SkillTitle, SkillList, SkillItem, SkillImage } from './SkillsStyles.js'
+import { skills } from '../../data/constants'
+
+
+
 
 const Skills = () => {
-    return <div>Skills</div>;
-};
+  return (
+    <Container id="skills">
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc>Here are some of my skills.
+        </Desc>
+        <SkillsContainer>
+          {skills.map((skill) => (
+            <Skill>
+              <SkillTitle>{skill.title}</SkillTitle>
+              <SkillList>
+                {skill.skills.map((item) => (
+                  <SkillItem>
+                    <SkillImage src={item.image}/>
+                    {item.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          ))}
 
-export default Skills;  
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Skills
